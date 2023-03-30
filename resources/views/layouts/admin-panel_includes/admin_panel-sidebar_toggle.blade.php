a459ac8ab5079b69d14d3dac93ac277e8a6bacb0
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-03-30 16:02:22 +0430

    admin panel layout created && admin panel templates created

---
+++ resources/views/layouts/admin-panel_includes/admin_panel-sidebar_toggle.blade.php
@@ -0,0 +1,4 @@
+
+<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+    <i class="fa fa-bars"></i>
+</button>
---
+++ resources/views/layouts/admin-panel_includes/nav_item/admin_panel-nav_item-tables.blade.php
@@ -0,0 +1,5 @@
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Tables</span></a>
+</li>
